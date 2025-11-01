f7d508e41433e7bc243199d00250ae0716a33b40
seungwon0113 <jungseungwon0113@gmail.com>
2025-11-01 21:49:28 +0900

    elasticsearch

--- products/views/customers/product_list.py
+++ products/views/customers/product_list.py
@@ -5,6 +5,7 @@ from django.views import View
 
 from categories.models import Category
 from products.models import Product
+from products.utils.elasticsearch.elastic_search import search_products
 
 
 class ProductListView(View):
@@ -16,11 +17,18 @@ class ProductListView(View):
         selected_category = None
         
         if search_query:
-            products = products.filter(
-                Q(name__icontains=search_query) |
-                Q(description__icontains=search_query) |
-                Q(categories__name__icontains=search_query)
-            ).distinct()
+            try:
+                product_ids = search_products(search_query)
+                if product_ids:
+                    products = products.filter(id__in=product_ids)
+                else:
+                    products = products.none()
+            except Exception:
+                products = products.filter(
+                    Q(name__icontains=search_query) |
+                    Q(description__icontains=search_query) |
+                    Q(categories__name__icontains=search_query)
+                ).distinct()
 
         if category_name:
             selected_category = Category.objects.filter(name=category_name).first()
